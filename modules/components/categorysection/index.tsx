9886ccc72a96bd2f0e58bb31cbc56dcb11c35163
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2024-06-10 16:40:02 +0530

    [UPD]: Basic file strcture updated!

---
+++ modules/components/categorysection/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <div>Category Section</div>
+  )
+}
+
+export default index
---
+++ modules/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default index
