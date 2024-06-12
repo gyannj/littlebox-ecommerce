35d8c8f5ee330164de6d9d9f019e06457f1943e9
Rituraj-Gautam <code.rhktj@slmail.me>
2024-06-12 18:12:04 +0530

    Changes in product card

--- modules/components/ProductCard/ProductCard.tsx
+++ modules/components/ProductCard/ProductCard.tsx
@@ -1,38 +1,34 @@
-
-
-
-
-
-
 import React from 'react'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import Image from 'next/image'
 
 
 function ProductCard() {
   return (
-    <div className='h-fit w-3/4 mx-auto my-auto bg-white'>
-      <div className="max-w-sm h-fit rounded overflow-hidden shadow-lg">
-        <img className="w-full" src="/apple-watch.png" alt="Sunset in the mountains" />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Apple Watch</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dol
-          </p>
-          <br></br>
-          <div className="font-bold text-xl mb-2">Price: Rs 1500</div>
+    <div className='h-3/4 w-3/4 mx-auto my-auto '>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+
+        
+        <div className='my-2 relative flex justify-center h-96 items-center' >
+        <Image src="/bookcover.jpg" fill alt="Sunset in the mountains" className='object-contain' />
         </div>
-        <div className="px-6 pt-4 pb-2">
+
+
+        {/* <img className="w-full" src="/apple-watch.png" alt="Sunset in the mountains" /> */}
+        <div className="px-4 py-4  text-textColor">
+          <div className="font-bold text-2xl mb-2">Apple Watch</div>
+          {/* <p className=" text-base">
+            Lorem ipsum dol
+          </p> */}
+          
+          <div className="font-medium text-base mb-2 mt-2">Rs 1500</div>
+        </div>
+
+
+        {/* <div className="px-6 pt-4 pb-2">
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded">
             Buy Now
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
 
--- modules/components/ProductSlider/productSlider.tsx
+++ modules/components/ProductSlider/productSlider.tsx
@@ -1,110 +1,54 @@
-
-
-import React from 'react';
+import React from 'react'
 import {
     Carousel,
     CarouselContent,
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-} from "@/components/ui/carousel";
-import ProductCard from '../ProductCard/ProductCard';
+} from "@/components/ui/carousel"
+import ProductCard from '../ProductCard/ProductCard'
+
 
 function ProductSlider() {
     return (
-        <div className='h-3/4 w-full bg-black'>
-            <Carousel className='h-full' opts={{ align: "start", loop: true }}>
-                <CarouselContent className='flex'>
+        
 
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+        <div className='basis-full h-3/4 w-full bg-dark-2 '>
+            
+
+            <Carousel 
+             opts={{
+                 align: "start",
+                 loop: true,
+                 }}
+                 
+                 >
+                <CarouselContent className=''>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
                     
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
                     
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
                     
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
+                    
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
             </Carousel>
-        </div>
-    );
+
+                    </div>
+        
+        
+    )
 }
 
-export default ProductSlider;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-// import {
-//     Carousel,
-//     CarouselContent,
-//     CarouselItem,
-//     CarouselNext,
-//     CarouselPrevious,
-// } from "@/components/ui/carousel"
-// import ProductCard from '../ProductCard/ProductCard'
-
-
-// function ProductSlider() {
-//     return (
-        
-
-//         <div className=' h-3/4 w-full bg-black '> 
-//         {/* basis-full */}
-            
-
-//             <Carousel className='h-full'
-//              opts={{
-//                  align: "start",
-//                  loop: true,
-//                  }}
-                 
-//                  >
-//                 <CarouselContent className='my-auto '>
-//                     {/* pt-20 */}
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-                    
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-                    
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-                    
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto "><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-//                     <CarouselItem className="md:basis-1/2 lg:basis-1/3 my-auto"><ProductCard/></CarouselItem>
-                    
-//                 </CarouselContent>
-//                 <CarouselPrevious />
-//                 <CarouselNext />
-//             </Carousel>
-
-//                     </div>
-        
-        
-//     )
-// }
-
-// export default ProductSlider
+export default ProductSlider
