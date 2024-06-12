f06fac966050587cd5b79eafdb7bbc61c4e8b2f0
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2024-06-12 16:38:06 +0530

    [UPD]: Navbar responsiveness code updated!

--- modules/components/navbar/NavMobile.tsx
+++ modules/components/navbar/NavMobile.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import React from 'react'
 
 
-const linkStyle = 'p-2 px-3 mx-3 hover:bg-dark-3 rounded-md text-3xl my-8';
+const linkStyle = 'p-2 px-3 mx-3 hover:bg-dark-3 rounded-md text-2xl my-8';
 
 const NavMobile = ({isOpen , closeMenu}:{isOpen:boolean , closeMenu:() => void}) => {
   return (
@@ -14,7 +14,7 @@ const NavMobile = ({isOpen , closeMenu}:{isOpen:boolean , closeMenu:() => void})
         </div>
 
         <div className='flex flex-col bg-dark-2 text-textColor align-between justify-center items-center'>
-            <Image src='/next.svg' alt='logo' width={120} height={80} className='bg-white rounded-md p-4 mt-4 mb-10' />
+            <Image src='/next.svg' alt='logo' width={200} height={80} className='bg-white rounded-md p-4 mt-4 mb-10' />
             <Link href='/' className={linkStyle} >
                 Home
             </Link>
