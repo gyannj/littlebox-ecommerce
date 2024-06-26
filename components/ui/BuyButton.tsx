1db438a8731a0d6fa44e2d4b341621ab7a28baef
gyannj <gyanjyotidas677@gmail.com>
2024-06-27 01:49:40 +0530

    Total Price added in cart page

--- components/ui/BuyButton.tsx
+++ components/ui/BuyButton.tsx
@@ -3,15 +3,19 @@ import { createOrder } from '@/app/cart/actions';
 import { cart_product } from '@/modules/shared/utils/types';
 import React from 'react'
 
-const BuyButton =(cartItems:any) => {
+interface BuyButtonProps {
+  cartItems: cart_product[];
+}
+
+const BuyButton = ({ cartItems }: BuyButtonProps) => {
 
   const handleProceedToCheckout = () => {
-    localStorage.setItem('cartItems', JSON.stringify(cartItems.cartItems));
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
   return (
     <div className=''>
        <div className="bg-dark-3 rounded-2xl  p-4 md:mt-10 h-32 w-8/12 md:w-auto ">
-       <div className="text-lg font-bold mb-4 text-textColor">Subtotal (4 items) : $500</div>
+       <div className="text-lg font-bold mb-4 text-textColor">SubTotal: USD { cartItems.reduce((acc , cartItem) => acc + (cartItem.price * cartItem.quantity) ,0)}</div>
        <div className='flex flex-col'>
          <form action="/checkout"  onSubmit={handleProceedToCheckout}>   
             <input  className="bg-dark-1 hover:bg-blue-600 text-textColor font-bold py-2 px-4 rounded cursor-pointer" type="submit" value="Proceed to Checkout"/>
