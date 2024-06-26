e28a42b2edf75acaa4189abe58b370730a27b24a
zayconik <zayconik@gmail.com>
2024-06-27 00:56:22 +0530

    [FIX]: Fixed error in cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,13 +6,13 @@ import { cart_product } from '@/modules/shared/utils/types';
 import { createOrder } from './actions';
 import BuyButton from '@/components/ui/BuyButton';
 
-const page =async () => {
-import CartCard from "@/modules/components/CartCard/CartCard";
-import React from "react";
-import {
-  getCartItems,
-  getProductsByCartItems,
-} from "../product/[...slug]/actions";
+// const page =async () => {
+// import CartCard from "@/modules/components/CartCard/CartCard";
+// import React from "react";
+// import {
+//   getCartItems,
+//   getProductsByCartItems,
+// } from "../product/[...slug]/actions";
 
 const page = async () => {
   const cartItems = await getProductsByCartItems();
@@ -40,3 +40,4 @@ const page = async () => {
 }
 
 export default page
+
