eb39fc9a23dee1462f6eb1e1db5c482b30b8a793
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-06-28 11:26:26 +0530

    [UPD]: Type cast errors fixed

--- modules/components/CheckoutFormm/CheckoutForm.tsx
+++ modules/components/CheckoutFormm/CheckoutForm.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react"
+"use client";
+import { useEffect, useRef, useState } from "react";
 import { createOrder } from "@/app/cart/actions";
 import { useRouter } from "next/navigation";
 
-
-import React from 'react'
+import React from "react";
 import { generatePDF } from "@/modules/shared/utils/pdfUtils";
 
 const placeholderStyle =
@@ -12,115 +12,108 @@ const placeholderStyle =
 const labelStyle = "block text-textColor text-sm font-bold mb-2";
 
 const CheckoutForm = () => {
+  const router = useRouter();
+  const [cartItems, setCartItems] = useState([]);
 
-
-    
-    const router = useRouter();
-    const [cartItems, setCartItems] = useState([]);
-
-    useEffect(() => {
-      const storedCartItems = localStorage.getItem('cartItems');
-      if (storedCartItems) {
-        setCartItems(JSON.parse(storedCartItems));
-      }
-    }, []);
-
-    const ref = useRef<HTMLFormElement>(null)
-    const handleFormSubmit =async (formData: FormData) => {
-        const formDataCopy = formData;
-        ref.current?.reset();
-        try {
-            const newOrder = await createOrder(cartItems,formDataCopy);
-            router.push('/confirmation');
-            if(newOrder !== 500){
-
-              generatePDF(newOrder);
-            }
-
-        } catch (error) {
-            console.log("Error Checking Out: ", error)
-        }
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
     }
+  }, []);
+
+  const ref = useRef<HTMLFormElement>(null);
+  const handleFormSubmit = async (formData: FormData) => {
+    const formDataCopy = formData;
+    ref.current?.reset();
+    try {
+      const newOrder = await createOrder(cartItems, formDataCopy);
+      router.push("/confirmation");
+      if (newOrder !== 500) {
+        generatePDF(newOrder);
+      }
+    } catch (error) {
+      console.log("Error Checking Out: ", error);
+    }
+  };
   return (
     <div className="max-w-md mx-auto m-10 p-6 bg-dark-2 flex flex-col rounded-lg shadow-md">
-    <form
-      ref={ref}
-      action={(formData) => {
-        handleFormSubmit(formData);
-      }}
-      className="flex flex-col"
-    >
-      <h2 className="text-2xl text-purpleText font-semibold mb-6 text-center">
-        Shipping Address
-      </h2>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          Address:
-          <input
-            type="text"
-            name="address.address"
-            className={placeholderStyle}
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          Pincode:
-          <input
-            type="string" // number to string
-            name="address.pincode"
-            className={placeholderStyle}
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          State:
-          <input
-            type="text"
-            name="address.state"
-            className={placeholderStyle}
-          />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          City:
-          <input
-            type="text"
-            name="address.city"
-            className={placeholderStyle}
-          />
-        </label>
-      </div>
-      <h2 className="text-2xl text-purpleText mt-16 font-semibold mb-6 text-center">
-        Personal Information
-      </h2>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          First Name:
-          <input type="text" name="first_name" className={placeholderStyle} />
-        </label>
-      </div>
-      <div className="mb-4">
-        <label className={labelStyle}>
-          Last Name:
-          <input type="text" name="last_name" className={placeholderStyle} />
-        </label>
-      </div>
-      <div className="flex flex-row justify-center items-center">
-        <button
-          type="submit"
-          className="bg-dark-3 py-2.5 px-4 size-fit rounded-lg hover:cursor-pointer hover:bg-slate-700 text-textColor"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-  )
-}
+      <form
+        ref={ref}
+        action={(formData) => {
+          handleFormSubmit(formData);
+        }}
+        className="flex flex-col"
+      >
+        <h2 className="text-2xl text-purpleText font-semibold mb-6 text-center">
+          Shipping Address
+        </h2>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            Address:
+            <input
+              type="text"
+              name="address.address"
+              className={placeholderStyle}
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            Pincode:
+            <input
+              type="string" // number to string
+              name="address.pincode"
+              className={placeholderStyle}
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            State:
+            <input
+              type="text"
+              name="address.state"
+              className={placeholderStyle}
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            City:
+            <input
+              type="text"
+              name="address.city"
+              className={placeholderStyle}
+            />
+          </label>
+        </div>
+        <h2 className="text-2xl text-purpleText mt-16 font-semibold mb-6 text-center">
+          Personal Information
+        </h2>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            First Name:
+            <input type="text" name="first_name" className={placeholderStyle} />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label className={labelStyle}>
+            Last Name:
+            <input type="text" name="last_name" className={placeholderStyle} />
+          </label>
+        </div>
+        <div className="flex flex-row justify-center items-center">
+          <button
+            type="submit"
+            className="bg-dark-3 py-2.5 px-4 size-fit rounded-lg hover:cursor-pointer hover:bg-slate-700 text-textColor"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default CheckoutForm
-
-
+export default CheckoutForm;
--- modules/components/categorysection/index.tsx
+++ modules/components/categorysection/index.tsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import ProductSlider from "@/modules/components/ProductSlider/ProductSlider";
-import { getProductsByCategoryId } from '@/app/actions';
-import { inventory } from '@/modules/shared/utils/types';
+import React from "react";
+import ProductSlider from "@/modules/components/ProductSlider/productSlider";
+import { getProductsByCategoryId } from "@/app/actions";
+import { inventory } from "@/modules/shared/utils/types";
 
 type Props = {
-  categoryName : string
-  categoryId : string
-}
+  categoryName: string;
+  categoryId: string;
+};
 
-const index = async ({categoryId , categoryName}: Props) => {
-  const data = await getProductsByCategoryId(categoryId)
-  console.log("data", data)
+const index = async ({ categoryId, categoryName }: Props) => {
+  const data = await getProductsByCategoryId(categoryId);
+  console.log("data", data);
   return (
     <>
       <div>
-        <h1 className='flex font-bold items-start text-textColor justify-start text-4xl pb-8'>{categoryName}</h1>
+        <h1 className="flex font-bold items-start text-textColor justify-start text-4xl pb-8">
+          {categoryName}
+        </h1>
       </div>
-      <ProductSlider products = {data} />
-      <div className='p-16'></div>
+      <ProductSlider products={data} />
+      <div className="p-16"></div>
       {/* <hr className='w-11/12 m-6 border border-gray-100'/> */}
     </>
     // <div className='flex justify-center items-center h-screen w-[300px] sm:w-11/12  bg-green-700'>
-  // </div>
-    
-  )
-}
+    // </div>
+  );
+};
 
-export default index
+export default index;
